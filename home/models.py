812c89ef9c882541779cdefcbbca6b8ff3600596
matto <matto@damniam.de>
2020-10-07 22:23:09 +0200

    init models

---
+++ home/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    StreamFieldPanel, 
+    PageChooserPanel
+)
+
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+
+from wagtail.embeds.blocks import EmbedBlock
+
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+
+class StartPage(Page):
+    show_in_menus_default = False
+    max_count = 1
+    parent_page_types = ['wagtailcore.page']
+    picture =  models.ForeignKey(
+        'wagtailimages.Image',
+        null = True,
+        blank = True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    next_page = models.ForeignKey(
+        #'wagtailcore.Page',
+        'home.StartPage',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('picture')
+    ]
+
+class WebPage(Page):
+    """ This is normal page"""
+    show_in_menus_default = True
+    # parent_page_types = ['home.StartPage']
+    body = StreamField (
+        [
+            ( 'paragraph', blocks.RichTextBlock(
+                features=['h1', 'h2', 'bold', 'italic', 'link', 'strikethrough'],
+                )
+            ),
+            ( 'HTML', blocks.RawHTMLBlock() ),
+            ( 'Embedded', EmbedBlock() ),
+            ( 'Picture', ImageChooserBlock()),
+        ]
+    )
+    content_panels = Page.content_panels + [
+        StreamFieldPanel ( 'body' ),
+    ]
+
+class Gallery(Page):
+    """ This page lists all works"""
+    show_in_menus_default = True
+    parent_page_types = ['home.StartPage']
+    subpage_types = ['WorkPage']
+    max_count = 1
+
+
+class WorkPage(WebPage):
+    """ works """
+    parent_page_types = ['Gallery']
+    subpage_types = []
+    coverimage = models.ForeignKey(
+        'wagtailimages.Image',
+        null = True,
+        blank = False,
+        on_delete = models.SET_NULL,
+        related_name = '+'
+    )
+    content_panels = [
+        ImageChooserPanel('coverimage'),
+    ] + WebPage.content_panels
+
